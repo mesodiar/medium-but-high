08713c4dc5d225a7625caf98f869d9abe5fe6632
mesodiar <mils.meso@gmail.com>
2020-07-16 17:02:53 +0700

    :sparkles: Add new app :koala:

---
+++ medium/story/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+class Story:
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
