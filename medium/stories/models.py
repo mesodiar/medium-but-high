69081d0661cac4f5600838537ce168039b45eec7
mesodiar <mils.meso@gmail.com>
2020-07-16 21:12:05 +0700

    1 show story in model :koala:

--- medium/stories/models.py
+++ medium/stories/models.py
@@ -1,16 +1,12 @@
 from django.conf import settings
 from django.db import models
 
-class Story:
+class Story(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
