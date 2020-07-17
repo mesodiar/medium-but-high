2959f9801c5b7ae71f93e35d294ab6a35341ade9
mesodiar <mils.meso@gmail.com>
2020-07-17 11:22:42 +0700

    2 Rendering story detail and index :koala:

--- medium/stories/views.py
+++ medium/stories/views.py
@@ -1,5 +1,17 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Story
 
 
 def index(request):
-    return HttpResponse("This is your story")
+    stories = Story.objects.all()    
+    context = {'stories': stories}
+    return render(request, 'stories/index.html', context)
+
+
+def detail(request, story_id):
+    story = Story.objects.get(id=story_id)
+
+    context = {'story': story}
+    return render(request, 'stories/detail.html', context)
