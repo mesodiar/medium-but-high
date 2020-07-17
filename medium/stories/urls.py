3c3591bb313157504b9cbdad9c2b8cc54b843836
mesodiar <mils.meso@gmail.com>
2020-07-17 17:25:38 +0700

    5 Add form :koala:

--- medium/stories/urls.py
+++ medium/stories/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:story_id>/', views.detail, name='detail'),
+    path('new-story/', views.new_story, name='new_story'),
 ]
+
--- medium/stories/views.py
+++ medium/stories/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
 
 from .models import Story
+from .forms import StoryForm
 
 
 def index(request):
@@ -15,3 +17,8 @@ def detail(request, story_id):
 
     context = {'story': story}
     return render(request, 'stories/detail.html', context)
+
+
+def new_story(request):
+    form = StoryForm()
+    return render(request, 'stories/new_story.html', {'form': form})
